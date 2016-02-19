4d771a0d14aae1e67308f85bcb02a8becf91367b
Douglas Casserleigh <casserleigh@gmail.com>
2016-02-19 16:29:52 -0700

    Added number_of_seconds.rb in Homework 3 topic branch
    [#114125561]

---
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+seconds_in_minute = 60
+minutes_in_hour = 60
+hours_in_day = 24
+days_in_week = 7
+weeks_in_year = 52
+years_at_age_20 = 20
+years_at_age_100 = 100
+
+
+puts "There are #{seconds_in_minute} seconds in a minute"
+puts "There are #{minutes_in_hour} minutes in an hour"
+puts "There are #{hours_in_day} hours in a day"
+puts "There are #{days_in_week} days in a week"
+puts "That means there are:"
+puts "#{seconds_in_minute * minutes_in_hour} seconds in an hour,"
+puts "#{seconds_in_minute * minutes_in_hour * hours_in_day} seconds in a day,"
+puts "#{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week} seconds in a week"
+puts "That means when you turn 20, you've been alive for #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * weeks_in_year * years_at_age_20} seconds
+and if you make it to 100 you will have lived #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * weeks_in_year * years_at_age_100} seconds.  Make them
+count!"
+
