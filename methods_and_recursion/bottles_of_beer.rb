7aa40893ecb255f44e8afebe4eb310181e502147
Douglas Casserleigh <casserleigh@gmail.com>
2016-02-27 14:25:36 -0700

    Altered code in bottles_of_beer.rb to satisfy travis test
    [#114627525]q

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -6,12 +6,12 @@ def bottles_of_beer
   while count > 1
 
     puts "#{count} bottles of beer on the wall, #{count} bottles of beer." +
-      " Take one down and pass it around, #{count} bottles of beer on the wall.\n\n"
+      " Take one down and pass it around, #{count -= 1} bottles of beer on the wall.\n\n"
     count -= 1
   end
 
   puts "#{count} bottle of beer on the wall, #{count} bottle of beer." +
-    " Take one down and pass it around, #{count} bottle of beer of beer on the wall.\n\n"
+    " Take one down and pass it around, #{count -= 1} bottles of beer of beer on the wall.\n\n"
 
   puts "No more bottles of beer on the wall, no more bottles of beer." +
     " Go to the store and buy some more, 99 bottles of beer on the wall."
