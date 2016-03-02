a5e9d06cd8709e70aa9b85786a9cb3d53c9eb062
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-02 13:41:50 -0700

    Fixed plurality issue within bottles_of_beer.rb strings
    [#114627525]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -3,11 +3,14 @@ def bottles_of_beer
 
   count = 99
 
-  while count > 1
+  while count > 2
     puts "#{count} bottles of beer on the wall, #{count} bottles of beer." +
       " Take one down and pass it around, #{count -= 1} bottles of beer on the wall.\n\n"
 
   end
+
+  puts "#{count} bottles of beer on the wall, #{count} bottles of beer." +
+    " Take one down and pass it around, #{count -= 1} bottle of beer on the wall.\n\n"
 
   puts "#{count} bottle of beer on the wall, #{count} bottle of beer." +
     " Take one down and pass it around, #{count -= 1} bottles of beer on the wall.\n\n"
