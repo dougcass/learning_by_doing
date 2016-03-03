66f05b4b5624fc670b10b27d22db06b08fc1189f
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-02 17:40:18 -0700

    Created fizz buzz array for array homework #2
    [#114374701]

---
+++ arrays/fizz_buzz_array.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env/ruby
+
+#!/usr/bin/env ruby
+fizz_buzz_array = []
+count = 0
+while count != 100
+  count += 1
+  if count % 3 == 0 && count % 5 == 0
+    fizz_buzz_array.push("FizzBuzz")
+  elsif count % 3 == 0
+    fizz_buzz_array.push("Fizz")
+  elsif count % 5 == 0
+    fizz_buzz_array.push("Buzz")
+  else
+    fizz_buzz_array.push(count)
+  end
+end
+
+puts fizz_buzz_array
+
