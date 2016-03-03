cb88f73cfb521a2e7ff53d26d21397b33b1ac45d
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-02 18:29:32 -0700

    Reformatted madlibs paragraph to present properly
    [#114627371]

---
+++ methods_and_recursion/mad_libs.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+def word(speech)
+  puts "Give me #{speech}"
+  gets.chomp
+end
+
+blank_1 = word("an adjective")
+blank_2 = word("an adjective")
+blank_3 = word("a noun")
+blank_4 = word("a noun")
+blank_5 = word("a plural noun")
+blank_6 = word("a game")
+blank_7 = word("a plural noun")
+blank_8 = word('a verb ending in "ing"')
+blank_9 = word('a verb ending in "ing"')
+blank_10 = word("a plural noun")
+blank_11 = word('a verb ending in "ing"')
+blank_12 = word("a noun")
+blank_13 = word("a plant")
+blank_14 = word("a part of the body")
+blank_15 = word("a place")
+blank_16 = word('a verb ending in "ing"')
+blank_17 = word("an adjective")
+blank_18 = word("a number")
+blank_19 =word("a plural noun")
+
+
+puts "A vacation is when you take a trip to some #{blank_1} place"
+puts "with your #{blank_2} family. Usually you go to some place"
+puts "that is near a/an #{blank_3} or up on a/an #{blank_4}."
+puts "A good vacation place is one where you can ride #{blank_5}"
+puts "or play #{blank_6} or go hunting for #{blank_7}. I like"
+puts "to spend my time #{blank_8} or #{blank_9}."
+puts "When parents go on a vacation, they spend their time eating"
+puts "three #{blank_10} a day, and fathers play golf, and mothers"
+puts "sit around #{blank_11}. Last summer, my little brother"
+puts "fell in a/an #{blank_12} and got poison #{blank_13} all"
+puts "over his #{blank_14}. My family is going to go to (the)"
+puts "#{blank_15}, and I will practice #{blank_16}. Parents"
+puts "need vacations more than kids because parents are always very"
+puts "#{blank_17} and because they have to work #{blank_18}"
+puts "hours every day all year making enough #{blank_19} to pay"
+puts "for the vacation."
