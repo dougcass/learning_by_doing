5f35329b0469bd18b600a76c10935b8ffde70875
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-04 11:27:42 -0700

    Fixed punctuation errors in number_of_seconds.rb
    [#114125561]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -14,10 +14,10 @@ puts "There are #{minutes_in_hour} minutes in an hour"
 puts "There are #{hours_in_day} hours in a day"
 puts "There are #{days_in_week} days in a week"
 puts "That means there are:"
-puts " #{seconds_in_minute * minutes_in_hour} seconds in an hour,"
-puts " #{seconds_in_minute * minutes_in_hour * hours_in_day} seconds in a day,"
-puts " #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week} seconds in a week"
-puts "That means when you turn 20, you've been alive for #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * weeks_in_year * years_at_age_20} seconds
-and if you make it to 100 you will have lived #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * weeks_in_year * years_at_age_100} seconds.  Make them
-count!"
+puts "  #{seconds_in_minute * minutes_in_hour} seconds in an hour,"
+puts "  #{seconds_in_minute * minutes_in_hour * hours_in_day} seconds in a day,"
+puts "  #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week} seconds in a week"
+puts "That means when you turn 20, you've been alive for #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * weeks_in_year * years_at_age_20} seconds,
+and if you make it to 100, you will have lived #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * weeks_in_year * years_at_age_100} seconds.
+Make them count!"
 
