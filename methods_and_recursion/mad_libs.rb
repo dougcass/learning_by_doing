ac4a98e3c1c0a9f1273a7a00852cc2ce3360e492
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-11 11:53:40 -0700

    Changed variable names from 'blank' to 'string'
    [#114627371]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -4,41 +4,40 @@ def word(speech)
   puts "Give me #{speech}"
   gets.chomp
 end
+string_1 = word("an adjective")
+string_2 = word("an adjective")
+string_3 = word("a noun")
+string_4 = word("a noun")
+string_5 = word("a plural noun")
+string_6 = word("a game")
+string_7 = word("a plural noun")
+string_8 = word('a verb ending in "ing"')
+string_9 = word('a verb ending in "ing"')
+string_10 = word("a plural noun")
+string_11 = word('a verb ending in "ing"')
+string_12 = word("a noun")
+string_13 = word("a plant")
+string_14 = word("a part of the body")
+string_15 = word("a place")
+string_16 = word('a verb ending in "ing"')
+string_17 = word("an adjective")
+string_18 = word("a number")
+string_19 =word("a plural noun")
 
-blank_1 = word("an adjective")
-blank_2 = word("an adjective")
-blank_3 = word("a noun")
-blank_4 = word("a noun")
-blank_5 = word("a plural noun")
-blank_6 = word("a game")
-blank_7 = word("a plural noun")
-blank_8 = word('a verb ending in "ing"')
-blank_9 = word('a verb ending in "ing"')
-blank_10 = word("a plural noun")
-blank_11 = word('a verb ending in "ing"')
-blank_12 = word("a noun")
-blank_13 = word("a plant")
-blank_14 = word("a part of the body")
-blank_15 = word("a place")
-blank_16 = word('a verb ending in "ing"')
-blank_17 = word("an adjective")
-blank_18 = word("a number")
-blank_19 =word("a plural noun")
 
-
-puts "A vacation is when you take a trip to some #{blank_1} place"
-puts "with your #{blank_2} family. Usually you go to some place"
-puts "that is near a/an #{blank_3} or up on a/an #{blank_4}."
-puts "A good vacation place is one where you can ride #{blank_5}"
-puts "or play #{blank_6} or go hunting for #{blank_7}. I like"
-puts "to spend my time #{blank_8} or #{blank_9}."
+puts "A vacation is when you take a trip to some #{string_1} place"
+puts "with your #{string_2} family. Usually you go to some place"
+puts "that is near a/an #{string_3} or up on a/an #{string_4}."
+puts "A good vacation place is one where you can ride #{string_5}"
+puts "or play #{string_6} or go hunting for #{string_7}. I like"
+puts "to spend my time #{string_8} or #{string_9}."
 puts "When parents go on a vacation, they spend their time eating"
-puts "three #{blank_10} a day, and fathers play golf, and mothers"
-puts "sit around #{blank_11}. Last summer, my little brother"
-puts "fell in a/an #{blank_12} and got poison #{blank_13} all"
-puts "over his #{blank_14}. My family is going to go to (the)"
-puts "#{blank_15}, and I will practice #{blank_16}. Parents"
+puts "three #{string_10} a day, and fathers play golf, and mothers"
+puts "sit around #{string_11}. Last summer, my little brother"
+puts "fell in a/an #{string_12} and got poison #{string_13} all"
+puts "over his #{string_14}. My family is going to go to (the)"
+puts "#{string_15}, and I will practice #{string_16}. Parents"
 puts "need vacations more than kids because parents are always very"
-puts "#{blank_17} and because they have to work #{blank_18}"
-puts "hours every day all year making enough #{blank_19} to pay"
+puts "#{string_17} and because they have to work #{string_18}"
+puts "hours every day all year making enough #{string_19} to pay"
 puts "for the vacation."
