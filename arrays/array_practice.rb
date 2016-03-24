9eac523497aef8ec5e898724e6f4cf738b24f68b
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-24 11:37:24 -0500

    Modified array methods for simpler syntax
    [#114903265]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -3,9 +3,9 @@
 #Create an array variable with the the numbers 1 through 10
 array = (1..10).to_a
 # 1...2...3...4...5...6...7...8...9...10...
-puts "#{array.join('...')}" + "..."
+puts array.join('...') + "..."
 # T-10, 9, 8, 7, 6, 5, 4, 3, 2, 1...  BLASTOFF!
-puts 'T-' + array.reverse.to_s.tr('[]',"") + '...  BLASTOFF!'
+puts 'T-' + array.reverse.join(', ') + '...  BLASTOFF!'
 # The last element is 10
 puts "The last element is #{array.last}"
 # The first element is 1
@@ -13,11 +13,11 @@ puts "The first element is #{array.first}"
 # The third element is 3
 puts "The third element is #{array[2]}"
 # The element with an index of 3 is 4
-puts "The element with an index of #{array[2]} is #{array[3]}"
+puts "The element with an index of 3 is #{array[3]}"
 # The second from last element is 9
-puts "The second from last element is #{array[8]}"
+puts "The second from last element is #{array[-2]}"
 # The first four elements are '1, 2, 3, 4'
-puts "The first four elements are '#{array.take(4).to_s.tr('[]',"")}'"
+puts "The first four elements are '#{array.take(4).join(', ')}'"
 # If we delete 5, 6 and 7 from the array, we're left with [1,2,3,4,8,9,10]
 #puts "If we delete #{array.delete_at(4)}, #{array.delete_at(4)} and #{array.delete_at(4)} from the array, we're left with [#{array.join(",")}] "
 puts "If we delete 5, 6 and 7 from the array, we're left with [#{array.delete_if{|array| array >4 && array < 8}.join(",")}] "
