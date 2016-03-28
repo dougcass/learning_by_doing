3a0d1b8c196b8361306267e585a3eb0f6a76853e
Douglas Casserleigh <casserleigh@gmail.com>
2016-03-28 15:13:01 -0600

    Edited code to operate recursively
    [#114627525]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,22 +1,34 @@
 #!/usr/bin/env ruby
-def bottles_of_beer
 
-  count = 99
 
-  while count > 2
-    puts "#{count} bottles of beer on the wall, #{count} bottles of beer." +
-      " Take one down and pass it around, #{count -= 1} bottles of beer on the wall.\n\n"
-
+def bottles_of_beer(number)
+  if number > 2
+    puts "#{number} bottles of beer on the wall, #{number} bottles of beer." +
+      " Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n\n"
+  elsif number == 2
+    puts "#{number} bottles of beer on the wall, #{number} bottles of beer." +
+      " Take one down and pass it around, #{number - 1} bottle of beer on the wall.\n\n"
+  elsif number == 1
+    puts "#{number} bottle of beer on the wall, #{number} bottle of beer." +
+      " Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n\n"
+  else
+    number == 0
+    puts " No more bottles of beer on the wall, no more bottles of beer." +
+      " Go to the store and buy some more, 99 bottles of beer on the wall."
+  end
+  if number > 0
+    bottles_of_beer(number-1)
   end
 
-  puts "#{count} bottles of beer on the wall, #{count} bottles of beer." +
-    " Take one down and pass it around, #{count -= 1} bottle of beer on the wall.\n\n"
-
-  puts "#{count} bottle of beer on the wall, #{count} bottle of beer." +
-    " Take one down and pass it around, #{count -= 1} bottles of beer on the wall.\n\n"
-
-  puts "No more bottles of beer on the wall, no more bottles of beer." +
-    " Go to the store and buy some more, 99 bottles of beer on the wall."
-
 end
-bottles_of_beer
+
+
+bottles_of_beer(99)
+
+
+
+
+
+
+
+
